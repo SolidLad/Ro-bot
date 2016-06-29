ba48ed46ff3e27217f5ff98c5e8ed9567bab2d84
SolidLad <alexsolidlad@gmail.com>
2016-06-29 11:08:14 -0400

    getting even closer with poll.

--- src/commands/Poll.java
+++ src/commands/Poll.java
@@ -1,25 +1,27 @@
 package commands;
 import commands.utils.Command;
 import net.dv8tion.jda.events.message.MessageReceivedEvent;
+import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;;
 import org.json.JSONObject;
+
 public class Poll implements Command {
-    private String responseString;
+    private int id = 1;
     @Override
     public void run(MessageReceivedEvent event, String[] args) {
         try {
             JSONObject json = jsonEncode(args);
 
             try (CloseableHttpClient httpClient = HttpClientBuilder.create().build()) {
-                HttpPost request = new HttpPost("http://strawpoll.me/api/v2/");
+                HttpPost request = new HttpPost("http://strawpoll.me/api/v2/polls");
                 StringEntity params = new StringEntity(json.toString());
                 request.addHeader("content-type", "application/json");
                 request.setEntity(params);
                 JSONObject response = new JSONObject(httpClient.execute(request));
-                responseString = response.toString();
+                id = response.getInt("id");
             } catch (Exception ex) {
                 ex.printStackTrace();
             }
@@ -28,16 +30,10 @@ public class Poll implements Command {
             e.printStackTrace();
         }
         finally {
-            int startindex = responseString.indexOf("id: ");
-            int endindex = responseString.indexOf(",");
-            System.out.println(responseString);
-            System.out.println(startindex+" "+endindex);
-            if (responseString!=null)
-                event.getTextChannel().sendMessage("Poll is up at www.strawpoll.me/"+responseString.substring(startindex,endindex));
-
+            event.getTextChannel().sendMessage("Poll is up at https://www.strawpoll.me/"+id);
         }
     }
-    public JSONObject jsonEncode(String[] args){
+    private JSONObject jsonEncode(String[] args){
         JSONObject obj = new JSONObject();
         obj.put("title", args[1]);
         for (int i = 2; i < args.length; i++) {
