34c22f3b3f7a5759d2f00f8674c326b3d08524b6
SolidLad <alexsolidlad@gmail.com>
2016-06-29 19:50:05 -0400

    updated !roll description

--- src/commands/text/Usage.java
+++ src/commands/text/Usage.java
@@ -12,7 +12,7 @@ public class Usage implements Command{
     private String[] descriptions = {"Bans a user. Usage: ```!ban <Username> <Days>```","Deafens a user. Usage: ```!deafen <Username>```",
             "Links a youtube video. Usage: ```!dream```", "Displays a list of commands Usage: ```!help```","Kicks a user. Usage: ```!kick <Username>```","Mutes a user. Usage: ```!mute <Username>```",
             "Plays one of patrick's sounds. Usage: ```!patrick <Sound>```","Plays a soundcloud track. Usage: ```!play <Track URL> <int Channel> <Float volume>```",
-            "Creates a poll with up to twenty options. Usage: ```!poll <Title>  <Option1> <Option2> <Option3> etc.","Rolls a six sided die. Usage: ```!roll```", "Makes the bot say something. Usage: ```!say <Message>```","Stops the currently play track and clears the queue.  Usage: ```!stop```",
+            "Creates a poll with up to twenty options. Usage: ```!poll <Title>  <Option1> <Option2> <Option3> etc.","Rolls a six sided die or a die with a specified number of sides. Usage: ```!roll <Sides>```", "Makes the bot say something. Usage: ```!say <Message>```","Stops the currently play track and clears the queue.  Usage: ```!stop```",
             "Unbans a user. Usage: ```!unban <Username>```", "Undeafens a user. Usage: ```!undeafen <Username>```", "Unmutes a user. Usage: ```!unmute <Username>```",
             "Displays a command description and usage. Usage: ```!usage <Command>```", "Prints a list of users and their online statuses. Usage: ```!users```"
     };
