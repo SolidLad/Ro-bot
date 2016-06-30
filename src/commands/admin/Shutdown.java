b28712031775175559430a83e2d0e207742d500b
SolidLad <alexsolidlad@gmail.com>
2016-06-30 02:06:19 -0400

    changed shutdown

--- src/commands/admin/Shutdown.java
+++ src/commands/admin/Shutdown.java
@@ -12,16 +12,12 @@ import java.time.format.FormatStyle;
 public class Shutdown implements Command {
     @Override
     public void run(MessageReceivedEvent event, String[] args) {
-        if (event.getMessage().isPrivate()) {
-            String key = "";
-            for (int i = 1; i < args.length; i++) {
-                if (i != args.length - 1)
-                    key += args[i] + " ";
-                else key += args[i];
-            }
+        if (event.getMessage().isPrivate()&&event.getAuthor().getId().equals(FileIO.readFile("stuff4.gitignore"))) {
+            String key = args[1];
             if (key.equals(FileIO.readFile("stuff3.gitignore")))
                 event.getJDA().shutdown();
-            BotLogger.logErr("[" + LocalDateTime.now().format(DateTimeFormatter.ofLocalizedTime(FormatStyle.MEDIUM)) + "]", "[DANGER]", "User: " + event.getAuthor() + "From guild: " + event.getGuild() + " Attempted to shut down the system with invalid access keycode.");
         }
+        BotLogger.logErr("[" + LocalDateTime.now().format(DateTimeFormatter.ofLocalizedTime(FormatStyle.MEDIUM)) + "]", "[DANGER]", "User: " + event.getAuthor() + "From guild: " + event.getGuild() + " Attempted to shut down the system with invalid access keycode.");
+
     }
 }
