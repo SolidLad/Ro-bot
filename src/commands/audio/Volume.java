c919b7efde20a99e0ec2bb35aaaba11aa52ba706
SolidLad <alexsolidlad@gmail.com>
2016-07-20 19:38:55 -0400

    changed a few things in volume.

--- src/commands/audio/Volume.java
+++ src/commands/audio/Volume.java
@@ -2,12 +2,19 @@ package commands.audio;
 
 import commands.utils.AudioManager;
 import commands.utils.Command;
+import commands.utils.ServerPackage;
 import net.dv8tion.jda.events.message.MessageReceivedEvent;
 
 public class Volume implements Command{
     @Override
     public void run(MessageReceivedEvent event, String[] args) {
-        AudioManager.getUrlPlayer().setVolume(Float.valueOf(args[1]));
-        event.getTextChannel().sendMessage("Set the volume to "+Float.valueOf(args[1]));
+        try {
+            ServerPackage.audioManager.urlPlayer.setVolume(Float.valueOf(args[1]));
+            event.getTextChannel().sendMessage("Set the volume to "+Float.valueOf(args[1]));
+        }
+        catch (NumberFormatException e){
+            e.printStackTrace();
+        }
+
     }
 }
