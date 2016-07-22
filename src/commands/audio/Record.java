651813692b63bc67b52bb74e36ba270a55a941ab
SolidLad <alexsooga@aim.com>
2016-07-22 06:37:18 -0400

    manually updated record so i dont lose it

--- src/commands/audio/Record.java
+++ src/commands/audio/Record.java
@@ -6,12 +6,13 @@ import net.dv8tion.jda.audio.UserAudio;
 import net.dv8tion.jda.entities.User;
 import net.dv8tion.jda.events.message.MessageReceivedEvent;
 import org.apache.commons.lang3.ArrayUtils;
-import javax.sound.sampled.AudioFileFormat;
-import javax.sound.sampled.AudioFormat;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
+import org.tritonus.sampled.file.WaveAudioFileWriter;
+
+import javax.sound.sampled.*;
 import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
 
 public class Record implements Command{
     private byte[] data;
@@ -47,7 +48,15 @@ public class Record implements Command{
             data = new byte[] {};
         }
         else {
-            writeWav(data);
+            try {
+                ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(data);
+                AudioInputStream audioInputStream = new AudioInputStream(byteArrayInputStream, AudioReceiveHandler.OUTPUT_FORMAT, data.length);
+                File file = new File("res\\test.wav");
+                new WaveAudioFileWriter().write(audioInputStream, AudioFileFormat.Type.WAVE, file);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
             event.getGuild().getAudioManager().closeAudioConnection();
             event.getGuild().getAudioManager().setReceivingHandler(new AudioReceiveHandler() {
                 @Override
@@ -76,29 +85,6 @@ public class Record implements Command{
 
                 }
             });
-        }
-    }
-    public void writeWav(byte[] pcm_data){
-        double L1 = 44100.0/240.0;
-        double L2 = 44100.0/245.0;
-        for (int i=0; i<pcm_data.length; i++) {
-            pcm_data[i] = (byte)(55*Math.sin((i/L1)*Math.PI*2));
-            pcm_data[i] += (byte)(55*Math.sin((i/L2)*Math.PI*2));
-        }
-
-        AudioFormat frmt = AudioReceiveHandler.OUTPUT_FORMAT;
-        AudioInputStream ais = new AudioInputStream(
-                new ByteArrayInputStream(pcm_data), frmt,
-                pcm_data.length / frmt.getFrameSize()
-        );
-
-        try {
-            AudioSystem.write(ais, AudioFileFormat.Type.WAVE, new
-                    File("res\\test.wav")
-            );
-        }
-        catch(Exception e) {
-            e.printStackTrace();
         }
     }
 }
