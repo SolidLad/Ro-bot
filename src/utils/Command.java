1b5a42519b7048ebb1f439f9438aca6888f7c256
John Broderick <johnbroderick12@gmail.com>
2016-07-30 19:51:27 -0400

    updated command

--- src/utils/Command.java
+++ src/utils/Command.java
@@ -5,4 +5,6 @@ import net.dv8tion.jda.events.message.MessageReceivedEvent;
 
 public interface Command {
     void run(MessageReceivedEvent event, String[] args) throws MalformedCommandException;
+
+    String getDescription();
 }
